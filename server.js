14322f574b7a7b97aa781ee95c32520fed6a88ed
Herbert Cuyugan <herbert.cuyugan@pentanasolutions.com>
2023-06-18 11:11:04 +1000

    connection to the mongodb, routes, adding product data, adding json middleware, insomnia

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 
 //in order for you to access this via a web browser, you will been to create routes
@@ -6,6 +7,28 @@ app.get("/", (req, res) => {
   res.send("Hello NODE API");
 });
 
-app.listen(3000, () => {
-  console.log(`Node is running on port 3000`);
+// this is when we needed to install nodemon because we cannot access the /blog
+app.get("/blog", (req, res) => {
+  res.send("Hello blog, My name is Sendoh");
 });
+
+// product route
+app.post("/products", (req, res) => {
+  console.log(req.body);
+  res.send(req.body);
+});
+
+// database connection
+mongoose
+  .connect(
+    "mongodb+srv://admin:hcrootadmin@hctestapi.rvwa7xj.mongodb.net/Node-API?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    console.log("connected to MongoDB");
+    app.listen(3000, () => {
+      console.log(`Node is running on port 3000`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
